61368f278b3067c7f59dae99773925158700e364
Eddi Xie <eddix808@gmail.com>
2026-02-24 13:29:13 -0800

    Added RC constants header used by the controller

---
+++ src/fc/header/RcConstants.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <cstdint>
+
+namespace fc::rc {
+
+constexpr std::uint16_t DRONE_MIN = 1000;
+constexpr std::uint16_t DRONE_MID = 1500;
+constexpr std::uint16_t DRONE_MAX = 2000;
+constexpr std::uint16_t DRONE_AUX_MIN = 1000;
+constexpr std::uint16_t DRONE_AUX_MAX = 2000;
+
+} // namespace fc::rc
